c26a616c094019b1e93bafa04d809db41a69e5cc
BarhamSlah <barhamsalahaddin@gmail.com>
2026-08-25 14:42:39 +0300

    Change logic Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,43 @@
 import java.util.Scanner;
+import java.util.Random;
 import java.util.ArrayList;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 void main() {
 
     ArrayList<Character> arraylist = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
 
+
+    String filePath = "src/Clubs.txt";
+ArrayList<String > Clubs = new ArrayList<>();
+
+
+   try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))){
+       String eachLine;
+
+       while ((eachLine = bufferedReader.readLine()) != null) {
+           if (!eachLine.trim().isEmpty()) {
+               Clubs.add(eachLine.trim()); // Adds each club from the file into the ArrayList
+           }
+       }
+
+   }catch (FileNotFoundException e){
+        System.out.println("File not found");
+   }catch (IOException e){
+       System.out.println("Something went wrong");
+   }catch (IllegalArgumentException e){
+       System.out.println(e.getMessage());
+   }
+
+    Random random = new Random();
+    String word = Clubs.get(random.nextInt(Clubs.size()));
+
     int wrongGuess = 0;
-    String word = "BUG";
+
 
     for (int i = 0; i < word.length(); i++) {
         arraylist.add('_');
@@ -28,7 +58,7 @@ void main() {
         System.out.println();
 
         System.out.print("Guess the letter ");
-        char guess = scanner.next().toUpperCase().charAt(0);
+        char guess = scanner.next().charAt(0);
 
         if (word.indexOf(guess) != -1) {
             for (int i = 0; i < word.length(); i++) {
@@ -36,10 +66,11 @@ void main() {
                     arraylist.set(i, guess);
                 }
             }
+
         } else {
             wrongGuess++;
         }
-
+        System.out.println(hangmanArt(wrongGuess));
     } // while loop stop here
 
     if(arraylist.contains('_')){
@@ -47,10 +78,45 @@ void main() {
         System.out.println("The word is " + word);
     }
 
-
-
-
-
-
     scanner.close();
+}
+
+static String hangmanArt(int wrongGuess) {
+
+  return  switch (wrongGuess) {
+
+      case 0 -> """
+                  
+                  
+                  """;
+      case 1 -> """
+                   o
+                  
+                  """;
+      case 2 -> """
+                   o
+                   |
+                  """;
+      case 3 -> """
+                   o
+                  /|
+                  """;
+      case 4 -> """
+                   o
+                  /|\\
+                  """;
+      case 5 -> """
+                   o
+                  /|\\
+                  /
+                  """;
+      case 6 -> """
+                   o
+                  /|\\
+                  / \\
+                  """;
+      default -> "";
+
+  };
+
 }
